4e5a1a411748a90b21a99eb6916b599b530dfa61
Favycoin <40722188+favycoin@users.noreply.github.com>
2018-07-01 02:30:48 +0500

    admin

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Favycoin-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "FavycoinURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
